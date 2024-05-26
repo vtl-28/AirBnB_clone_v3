23b5e28f5e7ee7e4344e93edd8f76c516b999045
root <root@Comfort>
2024-05-26 14:02:00 +0200

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,16 +17,9 @@ def stats():
     stats of all objs route
     :return: json of all objs
     """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
