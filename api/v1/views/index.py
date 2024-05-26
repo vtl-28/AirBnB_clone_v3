f189f1b7c369fcaeb3d6a112895cc91f8046eb75
root <root@Comfort>
2024-05-26 15:24:20 +0200

    revert

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,12 +18,15 @@ def stats():
     :return: json of all objs
     """
     data = {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User"),
-            }
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
 
-    return jsonify(data)
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
